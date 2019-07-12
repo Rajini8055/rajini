66c0eb631f1bbd770c75783a829b245e9653e064
Kumar <8055rk@gmail.com>
2019-07-12 17:00:16 -0400

    Update deleteIndex.sh

--- deleteIndex.sh
+++ deleteIndex.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+# $1 takes the file which we provide while executing
 filename=$1
+#initial value to count the lines
 n=1
+#while loop where index is the variable we use for looping
 while read index;
 do
 #reading each line from filename
 echo "In line No. $n : Deleting status of $index"
 n=$((n+1))
-        if [[ $line == *kibana* ]];
+#provide the pattern we want to exclude the indices from deletion
+        if [[ $line == *<pattern we want to exclude>* ]]; 
         then
-                echo " CAREFUL!!!! $line is kibana index."
+                echo " CAREFUL!!!! $index is sensitive index."
         else
-                curl -XDELETE "<<http://master node:--port number-->>/$index"
+            #command we want need to execute
+            curl -XDELETE "<<http://master node:--port number-->>/$index"
         fi
 done < $filename
